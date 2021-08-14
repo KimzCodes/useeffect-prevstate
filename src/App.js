f4c55bf2669571f5e9f37ac88fae2fb5e989f422
kareem nour <kimzcodes@gmail.com>
2021-08-14 05:28:54 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [term, setTerm] = useState('javascript');
+  //new setTrm2
+  const prevTermState = useRef();
+
+  useEffect(() => {
+    prevTermState.current = term;
+  });
+
+  const prevTerm = prevTermState.current;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <div className='my-3'>
+            <label htmlFor='exampleFormControlInput1' className='form-label'>
+              Search Input
+            </label>
+            <input
+              type='text'
+              className='form-control'
+              id='exampleFormControlInput1'
+              onChange={(e) => setTerm(e.target.value)}
+              value={term}
+            />
+          </div>
+          <p>Current term: {term}</p>
+          <p>prev term: {prevTerm}</p>
+        </div>
+      </div>
     </div>
   );
 }
